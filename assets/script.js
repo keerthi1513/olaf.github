db2554f8c5fc76dcca2181ccdc9fec95c2913555
keerthi.B <2022keerthi.b@vidyashilp.edu.in>
2025-01-28 09:21:47 +0530

    Delete assets directory

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-// Example for additional interactivity or future enhancements
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
