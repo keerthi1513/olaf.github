5adef4da3578f9be910457286b84a7b87e60ca5e
keerthi.B <2022keerthi.b@vidyashilp.edu.in>
2025-01-29 16:29:53 +0530

    Add files via upload

---
+++ Assignment one/script.js
@@ -0,0 +1,9 @@
+// Example for additional interactivity or future enhancements
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
